ab9676f90dc1bb5d51bcbd34272a90484d0c659a
JSeam <joshiaseam@gmail.com>
2017-06-01 20:39:43 +0800

    nn1 complete

--- Neural Networks I/machine-learning-ex3/ex3/predict.m
+++ Neural Networks I/machine-learning-ex3/ex3/predict.m
@@ -21,7 +21,14 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% add ones
+X = [ones(m,1) X];
 
+% Layer 1
+layer1 = sigmoid(X*Theta1');
+layer2 = [ones(m,1) layer1];
+layer3 = sigmoid(layer2*Theta2');
+[val p] = max(layer3,[],2);
 
 
 
